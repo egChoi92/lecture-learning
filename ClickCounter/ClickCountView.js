6479286d488e99a63f4e77bc1902b0c0a1f38573
agal2 <2013agal@gmail.com>
2021-09-02 14:06:57 +0900

    클릭 카운트 뷰 모듈 - 스펙1

---
+++ ClickCounter/ClickCountView.js
@@ -0,0 +1,13 @@
+var App = App || {};
+
+App.ClickCountView = (clickCounter, updateEl) => {
+    if (!clickCounter) throw Error('clickCounter');
+
+    if (!updateEl) throw Error('updateEl');
+
+    return {
+        updateView() {
+            updateEl.innerHTML = clickCounter.getValue();
+        }
+    }
+}
