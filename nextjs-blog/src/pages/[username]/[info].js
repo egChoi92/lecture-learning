06dc6e74ed6695896daf6e9ff0aef98d00afa263
egchoi <2013agal@gmail.com>
2023-05-27 22:44:45 +0900

    2)1-7. Next.js 기본 6(API Routes)

--- nextjs-blog/src/pages/[username]/[info].js
+++ nextjs-blog/src/pages/[username]/[info].js
@@ -6,10 +6,36 @@ import Layout from 'components/Layout';
 
 export default function UsernameInfo() {
   const router = useRouter();
-  const { username, info} = router.query;
+  const {username, info, uid} = router.query;
+  const [name, setName] = useState();
+  
+  // useEffect(() => {
+  //   fetch('/api/user'
+  //     ).then(res => { 
+  //       console.log('res: ', res);
+  //       return res.json()
+  //     }).then(data => {
+  //       setName(data.name)
+  //     })
+  // }, [])
+
+  useEffect(() => {
+    if (uid) {
+      fetch(`/api/user-info/${uid}`
+        ).then(res => { 
+          return res.json()
+        }).then(data => {
+          setName(data.name)
+          console.log('data: ', name);
+        })
+      
+    }
+  }, [uid])
+
   return (
     <>
       <h1 className="title">{username} : {info}</h1>
+      <p>Name: {name}</p>
     </>
   )
 }
