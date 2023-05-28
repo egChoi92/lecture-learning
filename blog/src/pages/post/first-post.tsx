afe590e2029b86fb7778d4bb93e3509cc832f54c
egchoi <2013agal@gmail.com>
2023-05-29 01:57:46 +0900

    3)1-1. 프로젝트 시작(Link Component _ Client-side Navigate)

---
+++ blog/src/pages/post/first-post.tsx
@@ -0,0 +1,5 @@
+export default function FirstPost() {
+    return ( 
+        <h1>첫번째 글</h1>
+    )
+};
---
+++ blog/src/pages/post/second-post.tsx
@@ -0,0 +1,5 @@
+export default function SecondPost() {
+    return ( 
+        <h1>두번째 글</h1>
+    )
+};
